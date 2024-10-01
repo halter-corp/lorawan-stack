c92a0684a5133717efa57f238eaf13ba010f50e3
Darya Plotnytska <d.plotnytska.s@gmail.com>
2024-10-01 14:17:36 +0200

    is: Add notification preferences migration and user field

--- pkg/ttnpb/user.pb.paths.fm.go
+++ pkg/ttnpb/user.pb.paths.fm.go
@@ -2,6 +2,13 @@
 
 package ttnpb
 
+var EmailNotificationPreferencesFieldPathsNested = []string{
+	"email_notification_types",
+}
+
+var EmailNotificationPreferencesFieldPathsTopLevel = []string{
+	"email_notification_types",
+}
 var UserConsolePreferencesFieldPathsNested = []string{
 	"console_theme",
 	"dashboard_layouts",
@@ -59,6 +66,8 @@ var UserFieldPathsNested = []string{
 	"created_at",
 	"deleted_at",
 	"description",
+	"email_notification_preferences",
+	"email_notification_preferences.email_notification_types",
 	"ids",
 	"ids.email",
 	"ids.user_id",
@@ -89,6 +98,7 @@ var UserFieldPathsTopLevel = []string{
 	"created_at",
 	"deleted_at",
 	"description",
+	"email_notification_preferences",
 	"ids",
 	"name",
 	"password",
@@ -167,6 +177,8 @@ var CreateUserRequestFieldPathsNested = []string{
 	"user.created_at",
 	"user.deleted_at",
 	"user.description",
+	"user.email_notification_preferences",
+	"user.email_notification_preferences.email_notification_types",
 	"user.ids",
 	"user.ids.email",
 	"user.ids.user_id",
@@ -223,6 +235,8 @@ var UpdateUserRequestFieldPathsNested = []string{
 	"user.created_at",
 	"user.deleted_at",
 	"user.description",
+	"user.email_notification_preferences",
+	"user.email_notification_preferences.email_notification_types",
 	"user.ids",
 	"user.ids.email",
 	"user.ids.user_id",
