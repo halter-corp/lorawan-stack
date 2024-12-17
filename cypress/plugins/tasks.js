2cc6a79725c20ea1f3a3374b2b4dcdf8d415dfe2
Darya Plotnytska <d.plotnytska.s@gmail.com>
2024-12-17 13:36:34 +0100

    dev: Test

--- cypress/plugins/tasks.js
+++ cypress/plugins/tasks.js
@@ -77,10 +77,7 @@ const sqlTask = on => {
     },
     dropAndSeedDatabase: async () => {
       const exec = util.promisify(childProcess.exec)
-      await Promise.all([
-        exec('tools/bin/mage dev:sqlRestore', { cwd: '..' }),
-        exec('tools/bin/mage dev:redisFlush', { cwd: '..' }),
-      ])
+      await Promise.all([exec('tools/bin/mage dev:sqlRestore', { cwd: '..' })])
       return null
     },
   })
