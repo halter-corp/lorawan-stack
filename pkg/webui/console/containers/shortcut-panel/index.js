1568d5ec531e758a50f348faaaf78aa92c144913
Darya Plotnytska <d.plotnytska.s@gmail.com>
2025-01-28 13:46:37 +0100

    console: Fix linting

--- pkg/webui/console/containers/shortcut-panel/index.js
+++ pkg/webui/console/containers/shortcut-panel/index.js
@@ -15,7 +15,6 @@
 import React from 'react'
 import { defineMessages } from 'react-intl'
 import { useDispatch, useSelector } from 'react-redux'
-import classNames from 'classnames'
 
 import { APPLICATION } from '@console/constants/entities'
 
